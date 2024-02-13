48ea8b492c0ffed243e97fd67dd644f0db444140
vision1v1 <vision1v1@163.com>
2024-02-13 22:44:52 +0800

    优化代码

--- train.py
+++ train.py
@@ -336,10 +336,10 @@ def calculate_acc(logit, labels, ignore_index=-100):
     logit = logit[..., :-1, :].contiguous().view(-1, logit.size(-1))
     labels = labels[..., 1:].contiguous().view(-1)
 
-    _, logit = logit.max(dim=-1)  # 对于每条数据，返回最大的index
+    _, logit_idx = logit.max(dim=-1)  # 对于每条数据，返回最大的index
     # 进行非运算，返回一个tensor，若labels的第i个位置为pad_id，则置为0，否则为1
     non_pad_mask = labels.ne(ignore_index)
-    n_correct = logit.eq(labels).masked_select(non_pad_mask).sum().item()
+    n_correct = logit_idx.eq(labels).masked_select(non_pad_mask).sum().item()
     n_word = non_pad_mask.sum().item()
     return n_correct, n_word
 
